ba095791ae9865cffbb0d93faa10620f549f3792
Jay <jremo25@outlook.com>
2023-11-23 03:24:58 +0000

    Update TF files

--- 02.Subnets.tf
+++ 02.Subnets.tf
@@ -1,33 +1,27 @@
-variable "public_subnet_cidrs" {
-  default = ["10.32.1.0/24", "10.32.2.0/24", "10.32.3.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  default = ["10.32.11.0/24", "10.32.12.0/24", "10.32.13.0/24"]
-}
-
 resource "aws_subnet" "public_subnet" {
-  count             = length(var.public_subnet_cidrs)
+  count             = 3
   vpc_id            = aws_vpc.app1.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.app1.cidr_block, 8, count.index + 1)  
   availability_zone = "eu-west-1${["a", "b", "c"][count.index]}"
   map_public_ip_on_launch = true
-  
+
   tags = {
     Name = "public-subnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  count             = length(var.private_subnet_cidrs)
+  count             = 3
   vpc_id            = aws_vpc.app1.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.app1.cidr_block, 8, count.index + 11)  
   availability_zone = "eu-west-1${["a", "b", "c"][count.index]}"
-  
+
   tags = {
     Name = "private-subnet-${count.index + 11}"
   }
 }
+
+
 
 resource "aws_internet_gateway" "app1_igw" {
   vpc_id = aws_vpc.app1.id
